0b54fa78d46e61ef3a0954f7dd24c196196b0863
niribareva22 <niribareva22@codingburgas.bg>
2025-06-10 01:45:25 +0300

    Start working on user dashboard

---
+++ MoviePass/include/pages/userDashboard.h
@@ -0,0 +1,8 @@
+#pragma once 
+
+#ifndef USER_DASHBOARD_H
+#define USER_DASHBOARD_H
+
+void userDashboard(const std::string& username);
+
+#endif
---
+++ MoviePass/src/pages/userDashboard.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "../../include/pages/userDashboard.h"
+
+void userDashboard(const std::string& username) {
+    std::cout << "=== Welcome to the User Dashboard, " << username << "! ===" << std::endl;
+   //choices
+}
