f611737a621ad29d7b56dd5d6ab3686643b6be57
niribareva22 <niribareva22@codingburgas.bg>
2025-06-24 13:35:55 +0300

    Add options to user dashboard

--- MoviePass/src/pages/userDashboard.cpp
+++ MoviePass/src/pages/userDashboard.cpp
@@ -4,5 +4,8 @@
 
 void userDashboard(const std::string& username) {
     std::cout << "=== Welcome to the User Dashboard, " << username << "! ===" << std::endl;
-   //choices
+    std::cout << "Please choose an option:" << std::endl;
+    std::cout << "1. View projections" << std::endl;
+    std::cout << "2. Buy a ticket" << std::endl;
+    std::cout << "3. Exit";
 }
